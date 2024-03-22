166bf62a991a6a759bc2ef8526f4a8fd62068dce
Mohammadi <Mohammadi.sa@eppendorf.de>
2024-03-22 11:13:40 +0100

    fix: frontend module

--- modules/frontend/src/App.tsx
+++ modules/frontend/src/App.tsx
@@ -2,22 +2,21 @@ import wasmLogo from "./assets/wasm.svg";
 import reactLogo from "./assets/react.svg";
 import { UniversalLib } from "@sadramoh/universal_lib/src/bindings/universal-lib/universal-lib";
 import { useState } from "react";
-import "./App.css";
 
-function App({ wasm }: { wasm: UniversalLib }) {
+function App({ wasm }: AppProps) {
   // A.1 -- state to store the message
   const [value, setValue] = useState<string>('');
   
+  // B.1 -- uncomment this use state to store the users name
+  // const [text, setText] = useState<string>('');
+  
   const handleClick = () => {
-    // A.2 -- add logic here to say hello to the user and show it in the UI using the `wasm` module
+    // A.2 -- add logic here to say "hello" and show it in the UI using the `wasm` module
+    // ...
+
+    // B.3 -- add logic to say hello with the input collected from the user
     // ...
   };
-
-  // B.2 -- uncomment this change handler
-  // const handleChange = () => {
-  //   // B.3 -- add logic to update the state on every keystroke
-  //   // ...
-  // }
   
   return (
     <>
@@ -30,11 +29,15 @@ function App({ wasm }: { wasm: UniversalLib }) {
           {/* A.3 -- show the value in the UI */}
         </b>
 
-        {/* B.1 -- uncomment this input */}
-        {/* <input type="text" onChange={(e) => setValue(e.currentTarget.value)} value={value} /> */}
+        {/* B.2 -- uncomment this input */}
+        {/* <input placeholder="User Name" type="text" onChange={(e) => setText(e.currentTarget.value)} value={text} /> */}
       </div>
     </>
   );
 }
+
+type AppProps = {
+  wasm: UniversalLib;
+};
 
 export default App;
